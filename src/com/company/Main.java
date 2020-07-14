efbb996f7c1dadc7162040b2a33c5cfd7698188e
Administrator <631615911@qq.com>
2020-07-14 11:23:16 +0800

    git项目提交

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi Word");
+        System.out.println("Hi Word ");
         System.out.println("Hi World");
     }
 }
