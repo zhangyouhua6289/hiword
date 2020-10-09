745b72c6224234756f90bc96576d97f5cdb5a09d
zhangyouhua <zhangyouhua@gwm.cn>
2020-10-09 14:50:50 +0800

    测试分支合并

---
+++ src/com/gittest/A.java
@@ -0,0 +1,15 @@
+package com.gittest;
+
+import java.util.logging.Logger;
+
+/**
+ * @author zhangyouhua
+ * @date 2020/10/9 14:43
+ */
+
+public class A {
+    private static final Logger logger = Logger.getGlobal();
+    public static void main(String[] args) {
+        logger.info("aaa");
+    }
+}
