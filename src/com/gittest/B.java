cbbf4022a8622109c89996d403d3110bf43147cb
bin <bin.com>
2020-11-15 12:35:59 +0800

    Revert "分支test1中修改class B"
    This reverts commit b1619e12

--- src/com/gittest/B.java
+++ src/com/gittest/B.java
@@ -8,10 +8,7 @@ public class B {
     public static void main(String[] args) {
         String a = "a,b,c";
         for (String s : a.split(",")) {
-            System.out.println(s+"changed");
+            System.out.println(s);
         }
-
-        System.out.println("这里进行了添加");
-
     }
 }
