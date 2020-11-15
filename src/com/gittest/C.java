bc834652cd2546b11b1d27bd9d0d4079f70a24bb
bin <bin.com>
2020-11-15 11:43:27 +0800

    创建test1分支

--- src/com/gittest/C.java
+++ src/com/gittest/C.java
@@ -2,6 +2,6 @@ package com.gittest;
 
 public class C {
     public static void main(String[] args) {
-        System.out.println("创建了一个test1分支");
+        System.out.println(" 创建了一个test1分支 ");
     }
 }
